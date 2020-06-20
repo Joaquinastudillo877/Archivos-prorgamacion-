85936ed5e5fe0630d00076a79ca559d2c421c9cf
Joaquinastudillo877 <66691077+Joaquinastudillo877@users.noreply.github.com>
2020-06-20 16:38:09 -0500

    Add files via upload

---
+++ tarea nombres edad y locacion.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 12:26:22 2020
+
+@author: joaquin astudillo
+"""
+Firstname=input("Dijite sus dos nombres")
+Lastname=input("Dijite sus dos apellidos")
+age=input("Dijite su edad")
+Location=input("Dijite la locacion de su vivienda")
+Space=" "
+print("Escaneo completado satisfactoriamente Nombre"+Space+Firstname+Space+"Apellido"+Space+Lastname+Space+"Edad"+Space+age+Space+"Vivienda"+Space+Location)
+
